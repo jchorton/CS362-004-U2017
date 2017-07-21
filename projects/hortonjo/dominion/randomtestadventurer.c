06c371141467d0d4eb2b723f9c145f290381eca1
Jonathan Horton <hortonjo@oregonstate.edu>
2017-07-21 12:40:56 -0700

    Progressing on assignment 4

--- projects/hortonjo/dominion/randomtestadventurer.c
+++ projects/hortonjo/dominion/randomtestadventurer.c
@@ -17,28 +17,28 @@
 #define DEBUG 0
 #define NOISY_TEST 1
 
-int checkDrawCard(int p, struct gameState *post) {
-	int r;
-
-	r = drawCard (p, post);
-	assert(r == 0);
-
-	return 0;
-}
-
-// We expect to gain up to 2 net cards so long as we have two treasury cards,
+// We expect to gain up to 2 additional cards so long as we have two treasury cards,
 // then discard the Adventurer card when done searching
-int checkAdventurerCard(int p, struct gameState *pre, struct gameState *post, int handPos) {
+int checkAdventurerCard(int p, struct gameState *post) {
 	
+	// Make a copy of the recieved gameState
+	struct gameState pre;
+	memcpy( &pre, post, sizeof(struct gameState) );
 	
-	// Call the card to test
-	actionAdventurer(&post, handPos);
+	// Choose random card position in hand
+	int handPos = floor(Random() * post->handCount[p]);
+
+	// Call Adventurer card!
+	int resultStatus;
+	resultStatus = actionAdventurer(post, handPos);
 
 	// Ensure correct values
-	assert(pre.handCount[p] + 1 == post.handCount[p]);
+	// TODO: Need to do a bool check that has no output, maybe
+	// Also keep in mind that this value should just be +1
+	assert(pre.handCount[p] + 2 == post->handCount[p]);
 
 
-	return 0;
+	return resultStatus;
 }
 
 
@@ -60,18 +60,32 @@ int main () {
 	SelectStream(1);
 	PutSeed(1);
 
-	int n, i, p, handPos;
+	int n, i, p;
 	for (n = 0; n < 2000; n++) {
-		// TODO: Need to somehow randomize initialize?
+		printf("Beginning test %d\n", n);
+
+		// Fills gameState with entirely random junk characters
 		for (i = 0; i < sizeof(struct gameState); i++) {
 			((char*)&G)[i] = floor(Random() * 256);
 		}
-		p = floor(Random() * 4);
+		p = floor(Random() * 4); // sets player number
+
+		// 27 cards, with enumed values of 0 to 26
+		// replace critical values to be a bit more limited
 		G.deckCount[p] = floor(Random() * MAX_DECK);
 		G.discardCount[p] = floor(Random() * MAX_DECK);
 		G.handCount[p] = floor(Random() * MAX_HAND);
-		handPos = floor(Random() * G.handCount[p]);
-		checkAdventurerCard(p, &G, handPos);
+		G.whoseTurn = p;
+		
+		
+		for (i = 0; i < G.handCount[p]; i++)
+			G.hand[p][i] = floor(Random() * 27);
+		for (i = 0; i < G.deckCount[p]; i++)
+			G.deck[p][i] = floor(Random() * 27);
+		for (i = 0; i < G.discardCount[p]; i++)
+			G.discard[p][i] = floor(Random() * 27);
+
+		checkAdventurerCard(p, &G);
 	}
 
 	printf ("ALL TESTS OK\n");
